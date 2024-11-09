4667747184f409baeb522a8269d03f5282220b12
Hernadil <hernadil@jedlik.hu>
2024-11-09 12:51:58 +0100

    valtozasok

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,11 @@ app.config['SECRET_KEY'] = "asd"
 #database settings
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123@localhost/dusza'
 
-from app.students import students
+from app.students import students_bp
+from app.dashboard import dashboard
 
-app.register_blueprint(students, url_prefix="/students")
+app.register_blueprint(students_bp, url_prefix="/students")
+app.register_blueprint(dashboard, url_prefix="/dashboard")
 
 if __name__ == '__main__':
     app.run()
--- app/dashboard.py
+++ app/dashboard.py
@@ -1,28 +1,29 @@
 from flask import Flask, Blueprint, render_template, request, session
-
 from flask_sqlalchemy import SQLAlchemy
-
 from datetime import datetime
+from tables import Languages, db
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import  DataRequired
+from forms import AddLanguageForm
 
-from app.tables import Languages, db # pyright: ignore
+dashboard_blueprint = Blueprint("dashboard", __name__, static_folder="static", template_folder="templates")
 
-dashboard = Blueprint("dashboard_bp", __name__, 
-                      static_folder="static", 
-                      template_folder="templates")
 
-@dashboard.route("/", methods=['GET'])
+
+@dashboard_blueprint.route("/", methods=['GET'])
 def dashboard():
     if session["permission"] == "admin":
         return render_template("dashboard.html")
 
 
-dashboard.route("/languages", methods=['GET', 'POST'])
+dashboard_blueprint.route("/languages", methods=['GET', 'POST'])
 def languages():
-    if request.method == 'GET' and session["permission"] == "admin":
-        return render_template("languages.html")
-    if request.method == 'POST':
-        language: str = request.form("language")
-        
-        new_language = Languages(name=language)
-        db.session.add(new_language)
-        db.session.commit()
+    form = AddLanguageForm()
+    language: str = None
+
+    if form.validate_on_submit():
+        language = form.language.data
+        return render_template("languages.html", content = language)
+
+    return render_template("languages.html", language = language, form = form)
